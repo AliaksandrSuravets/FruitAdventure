38badd585545328a10c1d5ffc1a610537a320a73
AliaksandrSuravets <BassMonroegg@gmail.com>
2023-11-24 18:37:41 +0300

    Add LevelService

--- Assets/Scripts/Services/LevelService.cs
+++ Assets/Scripts/Services/LevelService.cs
@@ -1,11 +1,41 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace FruitAdventure.Services
 {
     public class LevelService : MonoBehaviour
     {
-        
-        
+        #region Variables
+
+        [SerializeField] private GameObject _levelButton;
+        [SerializeField] private Transform _levelButtonParent;
+
+        #endregion
+
+        #region Unity lifecycle
+
+        private void Start()
+        {
+            for (int i = 1; i < SceneManager.sceneCountInBuildSettings; i++)
+            {
+                string sceneName = "Level" + i;
+
+                GameObject newButton = Instantiate(_levelButton, _levelButtonParent);
+                newButton.AddComponent<Button>().onClick.AddListener(() => LoadLevel(sceneName));
+            }
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public void LoadLevel(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        #endregion
     }
 }
